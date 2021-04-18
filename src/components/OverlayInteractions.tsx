abfe2dfda456274a7bc1897cb4106da365bac6c4
Kalen Goo <ktwmgoo@gmail.com>
2021-04-18 14:01:41 -0700

    Refactored icons, set-up for ShowInfo component

--- src/components/OverlayInteractions.tsx
+++ src/components/OverlayInteractions.tsx
@@ -1,11 +1,6 @@
 /** @jsxImportSource @emotion/react */
 import { css } from "@emotion/react";
-import { Link, ArrowRight, Info } from "react-feather";
-import {
-  Interaction,
-  LinkInteraction,
-  TraverseInteraction,
-} from "../types/Interactions";
+import { Interaction, InfoInteraction } from "../types/Interactions";
 
 const buttonCss = css`
   padding: 7px 15px;
@@ -35,45 +30,57 @@ const buttonCss = css`
   }
 `;
 
-// returns the proper icon for each interaction
-function returnIcon(interaction: Interaction) {
-  if (interaction instanceof LinkInteraction) {
-    return Link;
-  } else if (interaction instanceof TraverseInteraction) {
-    return ArrowRight;
+const infoCss = css`
+  padding: 7px 15px;
+  background-color: #0099ff;
+  color: white;
+  border: none;
+  width: 100%;
+  cursor: pointer;
+  transition: transform 0.2s;
+  border-radius: 10px;
+  margin: 5px 0;
+  font-size: 15px;
+
+  &:active {
+    transform: scale(0.95);
   }
-  // default to the Info icon
-  return Info;
-}
 
-// opens a new tab with the given link
-function linkOnClick(link: string) {
-  window.open(link, "_blank");
-}
-
-// returns the proper onClick method for each interaction
-function returnOnClick(interaction: Interaction) {
-  if (interaction instanceof LinkInteraction) {
-    return () => linkOnClick(interaction.url);
-  } else {
-    return () => {};
+  svg,
+  span {
+    display: inline-block;
+    vertical-align: middle;
   }
-}
 
+  span {
+    margin-left: 5px;
+    font-weight: 700;
+  }
+`;
 interface OverlayInteractionsProps {
   interactions: Interaction[];
 }
 
 export default function OverlayInteractions(args: OverlayInteractionsProps) {
   let { interactions } = args;
-  let buttons = interactions.map((interaction, key) => {
-    const Icon = returnIcon(interaction);
-    let onClick = returnOnClick(interaction);
+  let buttons = interactions.map((interaction: Interaction, key) => {
+    const { Icon, onClick } = interaction;
+    const isVisible = false;
+
+    // grab information if InfoInteraction
+    if (interaction instanceof InfoInteraction) {
+      var { information } = interaction;
+      // TODO: Create a ShowInfo component with useState to toggle its appearance
+    }
+
     return (
-      <button css={buttonCss} key={key} onClick={onClick}>
-        <Icon />
-        <span>{interaction.buttonText}</span>
-      </button>
+      <>
+        <button css={buttonCss} key={key} onClick={onClick}>
+          <Icon />
+          <span>{interaction.buttonText}</span>
+        </button>
+        {information && isVisible && <div>information</div>}
+      </>
     );
   });
 
